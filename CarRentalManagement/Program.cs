34fbbb6c57202887f41a12d548f222e34870adbf
BoooSAMA <booosama0113@gmail.com>
2025-12-10 10:37:59 +0800

    Added:1.Authentication,2. Authorization,3. QuickGrid

--- CarRentalManagement/Program.cs
+++ CarRentalManagement/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<CarRentalManagementContext>(options =>
+builder.Services.AddDbContextFactory<CarRentalManagementContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddQuickGridEntityFrameworkAdapter();
